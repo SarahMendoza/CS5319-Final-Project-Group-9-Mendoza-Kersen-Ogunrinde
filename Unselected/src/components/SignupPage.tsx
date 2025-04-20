9bb54aeba80995a04dd7f85a5eed196e25c10ecc
TooshieTom <tkersen@smu.edu>
2025-04-19 21:23:24 -0500

    signup updated

--- Unselected/src/components/SignupPage.tsx
+++ Unselected/src/components/SignupPage.tsx
@@ -10,7 +10,7 @@ const SignupPage = () => {
   const handleSignup = async () => {
     if (username && password) {
       try {
-        const response = await axios.post("http://localhost:5000/api/signup", {
+        const response = await axios.post("http://localhost:5000/create-user", {
           username,
           password,
         });
